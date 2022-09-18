40017238c04799b8ea0f072baee948c941405c16
cesarlm05 <cesarlm05@gmail.com>
2022-09-18 08:53:06 -0300

    Primer Commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+gsap.from("#m1", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 100,
+})
+gsap.from("#m2", {
+    scrollTrigger : {
+        scrub: true
+    },
+    y: 50,
+})
+gsap.from("#t2", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -50,
+})
+gsap.from("#t1", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 50,
+})
+gsap.from("#man", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -210,
+})
+gsap.from("#plants", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: -50,
+})
+gsap.from(".text", {
+    scrollTrigger : {
+        scrub: true
+    },
+    x: 600,
+})
